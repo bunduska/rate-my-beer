7dbc92d1c1910e2648835534e62147056d8c2077
Toth Szabolcs <szabolcstoth@outlook.com>
2021-12-08 20:11:52 +0000

    backend: adding e2e tests for user endpoints

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -9,6 +9,23 @@ import { User } from './../src/models/user.model';
 import { Beer } from './../src/models/beer.model';
 import { Repository } from 'typeorm';
 
+const newUser: User = new User('uj', 'mi@mi.com', 'ninini');
+const newBeer: Beer = {
+  id: 1,
+  name: 'newbeer',
+  rating: 2,
+  type: 'világos',
+  abv: 4,
+  brewery: 'Kanizsai',
+  country: 'Madzsar',
+  city: 'Nagykanizsa',
+  imageUrl: '',
+  comment: 'ittam már jobbat is',
+  date: undefined,
+  user: undefined,
+} as Beer;
+const TOKEN_PREFIX = 'Bearer';
+
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
@@ -51,7 +68,7 @@ describe('AppController (e2e)', () => {
     await app.close();
   });
 
-  it('should get a greeting message for root endpoint)', async () => {
+  it('should get a greeting message from root endpoint)', async () => {
     const response = await request(app.getHttpServer()).get('/');
     expect(response.statusCode).toBe(200);
     expect(response.text).toBe(`Rate Beer App's backend database greets you!`);
@@ -60,14 +77,14 @@ describe('AppController (e2e)', () => {
   it('should register a user', async () => {
     const response = await request(app.getHttpServer())
       .post('/register')
-      .send(new User('uj', 'mi@mi.com', 'ninini'));
+      .send(newUser);
     expect(response.statusCode).toBe(201);
   });
 
   it('should not register a new user with an existing e-mail address', async () => {
     const response = await request(app.getHttpServer())
       .post('/register')
-      .send(new User('uj', 'mi@mi.com', 'ninini'));
+      .send(newUser);
     expect(response.body).toStrictEqual({ message: 'Email is already taken!' });
   });
 
@@ -81,19 +98,142 @@ describe('AppController (e2e)', () => {
   it('should not log in a user without e-mail validation', async () => {
     const response = await request(app.getHttpServer())
       .post('/login')
-      .send({ email: 'mi@mi.com', password: 'nincs' });
+      .send({ email: newUser.email, password: 'nincs' });
     expect(response.body).toStrictEqual({
       message: 'Please validate your e-mail!',
     });
   });
 
-  it('should log in the user after an e-mail validation', async () => {
+  it('should log in the user after e-mail validation', async () => {
     const userRepo = app.get<Repository<User>>(getRepositoryToken(User));
     await userRepo.query("UPDATE ratemybeer_test.user SET isValidated = '1'");
 
     const response = await request(app.getHttpServer())
       .post('/login')
-      .send({ email: 'mi@mi.com', password: 'ninini' });
+      .send({ email: newUser.email, password: newUser.password });
     expect(response.text).toContain('token');
+  });
+
+  it('should be able to save a new beer for an existing user', async () => {
+    const responseForLogin = await request(app.getHttpServer())
+      .post('/login')
+      .send({ email: newUser.email, password: newUser.password });
+    const token: { token: string } = responseForLogin.body;
+
+    const response = await request(app.getHttpServer())
+      .post('/savebeer')
+      .send(newBeer)
+      .set('Authorization', `${TOKEN_PREFIX} ${token.token} `);
+
+    expect(response.text).toContain('Beer entry succesfully saved');
+  });
+
+  it('should be able to return the beerlist of an existing user', async () => {
+    const responseForLogin = await request(app.getHttpServer())
+      .post('/login')
+      .send({ email: newUser.email, password: newUser.password });
+    const token: { token: string } = responseForLogin.body;
+
+    const response = await request(app.getHttpServer())
+      .get('/beerlist')
+      .set('Authorization', `${TOKEN_PREFIX} ${token.token} `);
+
+    const beerlist: Beer[] = response.body as Beer[];
+
+    expect(beerlist.length).toBeGreaterThan(0);
+  });
+
+  it('should be able to update an existing beer', async () => {
+    const responseForLogin = await request(app.getHttpServer())
+      .post('/login')
+      .send({ email: newUser.email, password: newUser.password });
+
+    const token: { token: string } = responseForLogin.body;
+
+    newBeer.rating = 5;
+    newBeer.comment = 'nem vót az olyan rossz';
+
+    await request(app.getHttpServer())
+      .post('/savebeer')
+      .send(newBeer)
+      .set('Authorization', `${TOKEN_PREFIX} ${token.token} `);
+
+    const response = await request(app.getHttpServer())
+      .get('/beerlist')
+      .set('Authorization', `${TOKEN_PREFIX} ${token.token} `);
+
+    const beerlist: Beer[] = response.body as Beer[];
+    expect(beerlist[0].rating).toBe(5);
+    expect(beerlist[0].comment).toBe('nem vót az olyan rossz');
+  });
+
+  it('should be able to delete an existing beer', async () => {
+    const responseForLogin = await request(app.getHttpServer())
+      .post('/login')
+      .send({ email: newUser.email, password: newUser.password });
+    const token: { token: string } = responseForLogin.body;
+
+    const response = await request(app.getHttpServer())
+      .get('/beerlist')
+      .set('Authorization', `${TOKEN_PREFIX} ${token.token} `);
+    const beerlist: Beer[] = response.body as Beer[];
+
+    const responseToDelete = await request(app.getHttpServer())
+      .delete('/deletebeer')
+      .send(beerlist[0])
+      .set('Authorization', `${TOKEN_PREFIX} ${token.token} `);
+
+    expect(responseToDelete.text).toContain('Beer entry was deleted.');
+  });
+
+  it('should allow only to an admin to update a user', async () => {
+    const responseForLogin = await request(app.getHttpServer())
+      .post('/login')
+      .send({ email: newUser.email, password: newUser.password });
+    const token: { token: string } = responseForLogin.body;
+
+    newUser.isAdmin = true;
+
+    const response = await request(app.getHttpServer())
+      .post('/user/update')
+      .send(newUser)
+      .set('Authorization', `${TOKEN_PREFIX} ${token.token} `);
+
+    expect(response.status).toBe(401);
+    expect(response.text).toContain('Invalid token');
+  });
+
+  it('should allow to update a user for an admin', async () => {
+    const userRepo = app.get<Repository<User>>(getRepositoryToken(User));
+    await userRepo.query("UPDATE ratemybeer_test.user SET isAdmin= '1'");
+
+    const responseForLogin = await request(app.getHttpServer())
+      .post('/login')
+      .send({ email: newUser.email, password: newUser.password });
+    const token: { token: string } = responseForLogin.body;
+
+    const response = await request(app.getHttpServer())
+      .post('/user/update')
+      .send(newUser)
+      .set('Authorization', `${TOKEN_PREFIX} ${token.token} `);
+
+    expect(response.status).toBe(201);
+    expect(response.text).toContain('User entry succesfully saved');
+  });
+
+  it('should allow to update a user for an admin', async () => {
+    const responseForLogin = await request(app.getHttpServer())
+      .post('/login')
+      .send({ email: newUser.email, password: newUser.password });
+    const token: { token: string } = responseForLogin.body;
+
+    const response = await request(app.getHttpServer())
+      .get('/user/list')
+      .set('Authorization', `${TOKEN_PREFIX} ${token.token} `);
+
+    const userList: User[] = response.body as User[];
+
+    expect(response.status).toBe(200);
+    expect(userList.length).toBeGreaterThan(0);
   });
 });
